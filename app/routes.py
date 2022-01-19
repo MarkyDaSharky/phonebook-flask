64be565189f446c4590cec03c82ceffff90a496b
Mark Torres <m1911torres@gmail.com>
2022-01-19 09:28:31 -0600

    phonebook hw almost done

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+from flask import render_template, redirect, url_for
+from app.forms import RegisterForm
+
+
+
+@app.route('/')
+def index():
+    my_name = 'Mark'
+    my_city = 'Houston'
+    my_state = 'Texas'
+    return render_template('index.html', name=my_name, city=my_city, state=my_state)
+
+
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        phone_number = form.phone.data
+        address = form.address.data
+        print(name, phone_number, address)
+        return redirect(url_for('index'))
+    return render_template('register.html', form = form)
